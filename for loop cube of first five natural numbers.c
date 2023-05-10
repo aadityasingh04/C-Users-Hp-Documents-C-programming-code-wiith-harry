4399076a57760c4ce5cc1e9ef0d3421b49c87b4a
aadityasingh04 <112172316+aadityasingh04@users.noreply.github.com>
2023-05-10 19:53:32 +0545

    Add files via upload

---
+++ for loop cube of first five natural numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int num,i ;
+  printf("Enter the nth term  of the natural number");
+  scanf("%d",&num);
+  
+  for (i=1;i<=num;i++)
+  {
+    printf("\n %d*%d=%d",i,i,i);
+    
+   
+  }
+}
